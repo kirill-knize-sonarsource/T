89328c68be1486bfabb94b43e70a72c1712de816
Kirill Knize <kirill.knize@sonarsource.com>
2020-09-08 17:22:35 +0200

    SL notifications testing

---
+++ src/main/java/Person3.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Person3 {
+  static Random random = new Random();
+  static String[] names = {"Joe", "Jane", "Ann", "John"};
+  int age;
+  String name;
+  long timestamp = random.nextLong();
+
+  @Override
+  public String toString() {
+    return "Person{" +
+      "age=" + age +
+      ", name='" + name + '\'' +
+      ", timestamp=" + timestamp +
+      '}';
+  }
+
+
+}
